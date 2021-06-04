8466582cbe09caf2d2d01d963853baf81b387e4f
Kaspar Reisenbuk <kasparr2@users.noreply.github.com>
2021-06-05 00:52:12 +0300

    Teine kodutöö

---
+++ add_news.php
@@ -0,0 +1,67 @@
+<?php
+
+    require_once "../../../conf.php";
+    //echo $server_host;
+    $news_input_error = NULL;
+    //var_dump($_POST); //On olemas ka $_GET, selle käsuga saab vormi sisu oma lehel kuvada.
+     $news_title = NULL;
+     $news_content = NULL;
+    if(isset($_POST["news_submit"])){
+		if(empty($_POST["news_title_input"])){
+			$news_input_error = "Uudise pealkiri on puudu! ";
+		}else{ 														
+			$news_title = $_POST["news_title_input"];				
+		}
+		if(empty($_POST["news_content_input"])){
+			$news_input_error .= "Uudise sisu on puudu!";
+		}else{														
+			$news_content = $_POST["news_content_input"];			
+		}
+		if(empty($news_input_error)){
+			//salvestame andmebaasi
+			store_news($_POST["news_title_input"], $_POST["news_content_input"], $_POST["news_author_input"]);
+		}
+	}
+
+    function store_news($news_title, $news_content, $news_author){
+        //echo $news_title .$news_content .$news_author;
+        //loome andmebaasis serveriga ja baasiga ühenduse
+        $conn = new mysqli($GLOBALS["server_host"], $GLOBALS["server_user_name"], $GLOBALS["server_password"], $GLOBALS["database"]);
+        //määran kodeeringu
+        $conn -> set_charset ("utf8");
+        //valmistan ette SQL Käsu
+        $stmt = $conn -> prepare("INSERT INTO vr21_news (vr21_news_news_title, vr21_news_news_content, vr21_news_news_author) VALUES (?,?,?)");
+        echo $conn -> error;            //i on integer, s on string, d on deciaml.
+        $stmt -> bind_param("sss", $news_title, $news_content, $news_author);
+        $stmt -> execute();
+        $stmt -> close();
+        $conn -> close(); 
+    } 
+?>
+<!DOCTYPE html>
+<html lang="et">
+<head>
+    <meta charset="utf-8"><title>Veebirakendused ja nende loomine 2021</title>
+</head>
+<body>
+    <h1>Uudiste lisamine</h1>
+    <p>See leht on valminud õppetöö raames!</p>
+    <hr>
+    <form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  <! -- Post meethod saadab andmed ära nii, et lingil ei ole näha kogu sisu. -->                      
+        <label for="news_title_input">Uudise pealkiri</label>
+        <br>
+        <input type="text" id="news_title_input" name="news_title_input" placeholder="Uudise pealkiri" value="<?php echo $news_title; ?>">
+        <br>
+        <label for="news_content_input">Uudise sisu</label>
+        <br>
+        <textarea id="news_content_input" name="news_content_input" placeholder="Uudise sisu" rows="6" cols="40"><?php echo $news_content; ?></textarea>
+        <br>
+        <label for="news_author_input">Uudise lisaja nmi</label>
+        <br>
+        <input type="text" id="news_author_input" name="news_author_input" placeholder="Nimi">
+        <br>
+        <input type="submit" name="news_submit" value="Salvesta uudis!">
+    </form>
+    <p><?php echo $news_input_error; ?></p>
+</body>
+</html>
---
+++ show_news.php
@@ -0,0 +1,60 @@
+<?php
+
+    require_once "../../../conf.php";
+
+    function read_news(){
+        if(isset($_POST["show_news"])) {
+        $count_news = $_POST['count_news']; //Kui kasutaja sisestab numbri kuvatakse väljund
+        } else {                          //"BY DEFAULT" on 6
+            $count_news = 6;
+        }
+        //loome andmebaasis serveriga ja baasiga ühenduse
+        $conn = new mysqli($GLOBALS["server_host"], $GLOBALS["server_user_name"], $GLOBALS["server_password"], $GLOBALS["database"]);
+        //määran kodeeringu
+        $conn -> set_charset ("utf8");
+        //valmistan ette SQL Käsu
+        //Täiendan sql käsku nii, et uudised oleks id järgi kahanevas järjekorras
+        //Täiendan käsku veel LIMIT-iga
+        $stmt = $conn -> prepare("SELECT vr21_news_news_title, vr21_news_news_content, vr21_news_news_author, vr21_news_added FROM vr21_news ORDER BY vr21_news_id DESC LIMIT ?");
+        echo $conn -> error;
+        //i - integer   s - string   d - decimal
+        $stmt -> bind_result($news_title_from_db, $news_content_from_db, $news_author_from_db, $news_added_from_db);
+        $stmt -> bind_param("s", $count_news);
+        $stmt -> execute();
+        $raw_news_html = null;
+
+        while ($stmt -> fetch()){
+            $raw_news_html .= "\n <h2>" .$news_title_from_db ."</h2>";
+            $news_date = new DateTime($news_added_from_db);     //Lisan postitustele aja.
+            $raw_news_html .= "\n <p> Lisatud: " .$news_date->format("m.d.Y H:i") . "</p>"; //Kuvan aja välja pealkirja alla.
+            $raw_news_html .= "\n <p>" .nl2br($news_content_from_db) ."</p>";
+            $raw_news_html .= "\n <p>Edastas: ";
+            if(!empty($news_author_from_db)){
+                $raw_news_html .= $news_author_from_db;
+            } else {
+                $raw_news_html .= "Tundmatu reporter";
+            }
+            $raw_news_html .= "</p>";
+        }
+        $stmt -> close();
+        $conn -> close();
+        return $raw_news_html;
+    }
+    $news_html = read_news();
+?>
+<!DOCTYPE html>
+<html lang="et">
+<head>
+    <meta charset="utf-8"><title>Veebirakendused ja nende loomine 2021</title>
+</head>
+<body>
+    <h1>Uudiste lugemine</h1>
+    <p>See leht on valminud õppetöö raames!</p>
+    <form method="POST"> <!-- Sellega kuvan uudised välja. -->
+	<input type="number" min="0" max="10" value="" name="count_news">
+	<input type="submit" name="show_news" value="Kuva uudised">
+	</form>
+    <hr>
+    <?php echo $news_html; ?>
+</body>
+</html>
